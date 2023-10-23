06ed906668eff6860c0770aa897782d0ae22eafa
Adam John Lea <imnogeekdptcom@me.com>
2023-10-23 15:34:17 -0600

    begin building header

--- functions.php
+++ functions.php
@@ -6,7 +6,9 @@
 // Includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
 include(get_theme_file_path('/includes/front/head.php'));
+include(get_theme_file_path('/includes/setup.php'));
 
 // Hooks
 add_action('wp_enqueue_scripts', 'm_enqueue' );
 add_action('wp_head', 'm_head', 5);
+add_action('after_setup_theme', 'm_setup_theme');
---
+++ includes/setup.php
@@ -0,0 +1,12 @@
+<?php
+
+function m_setup_theme(): void
+{
+	add_theme_support('editor-styles');
+
+	add_editor_style([
+		'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
+		'assets/bootstrap-icons/bootstrap-icons.css',
+		'assets/public/index.css'
+	]);
+}
